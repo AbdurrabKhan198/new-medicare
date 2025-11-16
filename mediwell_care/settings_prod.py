957eb14a5eadae2b12ef97822ee49a12ae59a007
AbdurrabKhan198 <abdurrabkhan709@gmail.com>
2025-11-17 02:50:21 +0530

    changes

--- mediwell_care/settings_prod.py
+++ mediwell_care/settings_prod.py
@@ -8,7 +8,7 @@ import environ
 from .settings import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # Environment variables
 env = environ.Env(
